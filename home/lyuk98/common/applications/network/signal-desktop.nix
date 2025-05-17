10c1fcef7528ee7bb2a3b950ab30fedbf6a6ee11
이영욱 <pr@lyuk98.com>
2025-05-17 18:09:46 +0900

    Fix autostart entry for Signal Desktop
    Provide correct name

--- home/lyuk98/common/applications/network/signal-desktop.nix
+++ home/lyuk98/common/applications/network/signal-desktop.nix
@@ -13,6 +13,6 @@
     home.packages = [ pkgs.signal-desktop ];
 
     # Enable automatic startup of the application
-    xdg.autostart.entries = [ "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop" ];
+    xdg.autostart.entries = [ "${pkgs.signal-desktop}/share/applications/signal.desktop" ];
   };
 }
