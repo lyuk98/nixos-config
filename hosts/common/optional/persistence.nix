5b4d059ce6e065edfb7b0f1e0d499f75a520e866
이영욱 <pr@lyuk98.com>
2025-06-30 15:19:08 -0400

    Create impermanence configuration

---
+++ hosts/common/optional/persistence.nix
@@ -0,0 +1,19 @@
+{ inputs, ... }:
+{
+  imports = [ inputs.impermanence.nixosModules.impermanence ];
+
+  environment.persistence = {
+    # Enable persistence storage at /persist
+    "/persist" = {
+      # Prevent bind mounts from being shown as mounted storage
+      hideMounts = true;
+
+      # Create bind mounts for given directories
+      directories = [
+        "/var/lib/nixos"
+        "/var/lib/systemd"
+        "/var/log"
+      ];
+    };
+  };
+}
