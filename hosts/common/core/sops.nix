3001516c6d96dea965686d99534d34146ef73346
이영욱 <pr@lyuk98.com>
2025-06-30 22:38:05 -0400

    Add persistent file for sops-nix

--- hosts/common/core/sops.nix
+++ hosts/common/core/sops.nix
@@ -1,8 +1,35 @@
-{ inputs, lib, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
+let
+  ageKeyFile = "/var/lib/sops-nix/keys.txt";
+in
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
   # Specify path of the age key to decrypt secrets with
   # This file needs to be present to decrypt secrets during activation
-  sops.age.keyFile = lib.mkDefault "/var/lib/sops-nix/keys.txt";
+  sops.age.keyFile = lib.mkDefault "${
+    # Specify persisted directory if Impermanence is used
+    # since the key will be used during early boot
+    if (config.environment ? persistence) then "/persist" else ""
+  }${ageKeyFile}";
+
+  # Use persistence if enabled
+  environment = lib.optionalAttrs (config.environment ? persistence) {
+    persistence."/persist" = {
+      files = [
+        # Keep age key
+        {
+          file = ageKeyFile;
+          parentDirectory = {
+            mode = "0600";
+          };
+        }
+      ];
+    };
+  };
 }
