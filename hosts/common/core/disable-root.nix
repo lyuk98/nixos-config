15ec760936d1fda0f13a6577e9fce1bf9ddc40d1
이영욱 <pr@lyuk98.com>
2025-07-07 15:20:21 -0400

    Prevent modifications to root password
    Use mkForce to prevent external configurations from setting root
    password

--- hosts/common/core/disable-root.nix
+++ hosts/common/core/disable-root.nix
@@ -1,4 +1,13 @@
+{ lib, ... }:
 {
   # Lock the root user
-  users.users.root.hashedPassword = "!";
+  users.users.root = {
+    hashedPassword = lib.mkForce "!";
+
+    # Unset other options to prevent multiple declarations
+    initialHashedPassword = lib.mkForce null;
+    initialPassword = lib.mkForce null;
+    password = lib.mkForce null;
+    hashedPasswordFile = lib.mkForce null;
+  };
 }
