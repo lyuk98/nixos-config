f26fd60793231fd0dd5755b5220145e1b42ed7c3
이영욱 <pr@lyuk98.com>
2025-08-08 16:59:17 -0400

    Allow SSH logins to root users

--- hosts/common/optional/sshd.nix
+++ hosts/common/optional/sshd.nix
@@ -5,10 +5,15 @@
     enable = true;
 
     settings = lib.mkIf config.services.openssh.enable {
-      # Prevent root users from logging in
-      PermitRootLogin = "no";
+      # Prevent logins to root users using password
+      PermitRootLogin = lib.mkDefault "prohibit-password";
       # Disallow password-based authentication
       PasswordAuthentication = lib.mkDefault false;
     };
   };
+
+  # Add authorized SSH keys for root
+  users.users.root.openssh.authorizedKeys.keyFiles = lib.mkDefault [
+    ../../../home/lyuk98/id_ed25519.pub
+  ];
 }
