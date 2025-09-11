b5f19dd294bea45c06844806bac7040c3c5629cd
이영욱 <pr@lyuk98.com>
2025-09-11 12:20:03 -0400

    Conditionally skip tests for Tailscale package
    Some kernel versions fail to build the package and, as a result, tests
    are skipped on those versions.

--- hosts/common/core/tailscale.nix
+++ hosts/common/core/tailscale.nix
@@ -1,8 +1,25 @@
-{ lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   services.tailscale = {
     # Enable Tailscale client daemon
     enable = true;
+
+    # Override Tailscale package to skip tests that affect some kernel versions
+    # https://github.com/NixOS/nixpkgs/issues/438765
+    package =
+      let
+        version = config.boot.kernelPackages.kernel.version;
+      in
+      lib.mkIf (
+        (lib.versionAtLeast version "6.6.102" && lib.versionOlder version "6.6.105")
+        || (lib.versionAtLeast version "6.12.42" && lib.versionOlder version "6.12.46")
+        || (lib.versionAtLeast version "6.16.1" && lib.versionOlder version "6.16.6")
+      ) (pkgs.tailscale.overrideAttrs { doCheck = false; });
 
     # Open firewall for the port to listen on
     openFirewall = true;
