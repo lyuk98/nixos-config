c7e2959a0e838dd357a634a2f0d6d6526c52155d
이영욱 <pr@lyuk98.com>
2025-09-10 23:00:15 -0400

    Create options to change autostart settings
    Create autostart options for existing applications

--- home/lyuk98/common/applications/network/proton-vpn.nix
+++ home/lyuk98/common/applications/network/proton-vpn.nix
@@ -5,14 +5,43 @@
   ...
 }:
 {
-  # Create option to enable Proton VPN
-  options.applications.network.proton-vpn.enable = lib.mkEnableOption "Proton VPN";
+  options.applications.network.proton-vpn = {
+    # Create option to enable Proton VPN
+    enable = lib.mkEnableOption "Proton VPN";
 
-  config = lib.mkIf config.applications.network.proton-vpn.enable {
-    # Add packages
-    home.packages = [ pkgs.protonvpn-gui ];
-
-    # Enable automatic startup of the application
-    xdg.autostart.entries = [ "${pkgs.protonvpn-gui}/share/applications/protonvpn-app.desktop" ];
+    # Create option to enable autostart for Proton VPN
+    autostart.enable = lib.mkOption {
+      default = true;
+      example = false;
+      description = "Whether to enable autostart for Proton VPN";
+      type = lib.types.bool;
+    };
   };
+
+  config =
+    let
+      pkg = pkgs.protonvpn-gui;
+    in
+    lib.mkIf config.applications.network.proton-vpn.enable {
+      # Add packages
+      home.packages = [ pkg ];
+
+      # Enable automatic startup of the application
+      systemd.user.services.proton-vpn-autostart =
+        lib.mkIf config.applications.network.proton-vpn.autostart.enable
+          {
+            Unit = {
+              Description = "Automatically start Proton VPN upon startup";
+              After = [ "graphical-session.target" ];
+            };
+            Install = {
+              WantedBy = [ "graphical-session.target" ];
+            };
+            Service = {
+              Type = "simple";
+              ExecStartPre = "${pkgs.networkmanager}/bin/nm-online";
+              ExecStart = "${pkg}/bin/protonvpn-app";
+            };
+          };
+    };
 }
--- home/lyuk98/common/applications/network/signal-desktop.nix
+++ home/lyuk98/common/applications/network/signal-desktop.nix
@@ -5,14 +5,28 @@
   ...
 }:
 {
-  # Create option to enable Signal Desktop
-  options.applications.network.signal-desktop.enable = lib.mkEnableOption "Signal Desktop";
+  options.applications.network.signal-desktop = {
+    # Create option to enable Signal Desktop
+    enable = lib.mkEnableOption "Signal Desktop";
 
-  config = lib.mkIf config.applications.network.signal-desktop.enable {
-    # Add Signal Desktop
-    home.packages = [ pkgs.signal-desktop ];
-
-    # Enable automatic startup of the application
-    xdg.autostart.entries = [ "${pkgs.signal-desktop}/share/applications/signal.desktop" ];
+    # Create option to enable autostart for Signal Desktop
+    autostart.enable = lib.mkOption {
+      default = true;
+      example = false;
+      description = "Whether to enable autostart for Signal Desktop";
+      type = lib.types.bool;
+    };
   };
+
+  config =
+    let
+      pkg = pkgs.signal-desktop;
+    in
+    lib.mkIf config.applications.network.signal-desktop.enable {
+      # Add Signal Desktop
+      home.packages = [ pkg ];
+
+      # Enable automatic startup of the application
+      xdg.autostart.entries = lib.optional config.applications.network.signal-desktop.autostart.enable "${pkg}/share/applications/signal.desktop";
+    };
 }
