47be026fc15500f0632dc0b3d608a060e4bbdfc9
이영욱 <pr@lyuk98.com>
2025-10-19 12:34:03 +0900

    Set persistent directory for vnStat

--- hosts/common/optional/vnstat.nix
+++ hosts/common/optional/vnstat.nix
@@ -1,4 +1,10 @@
+{ lib, config, ... }:
 {
   # Enable network usage statistics with vnStat
   services.vnstat.enable = true;
+
+  # Make data directory persistent if Impermanence is enabled
+  environment = lib.optionalAttrs (config.environment ? persistence) {
+    persistence."/persist".directories = [ "/var/lib/vnstat" ];
+  };
 }
