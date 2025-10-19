4b3eea0ab4a000f02387d65d8352c82cbd84ad9d
이영욱 <pr@lyuk98.com>
2025-10-19 14:47:15 +0900

    Set persistent directory for Flatpak

--- hosts/common/optional/flatpak.nix
+++ hosts/common/optional/flatpak.nix
@@ -1,4 +1,9 @@
-{
+{lib, config, ...}: {
   # Enable Flatpak
   services.flatpak.enable = true;
+
+  # Make data directory persistent if Impermanence is enabled
+  environment = lib.optionalAttrs (config.environment ? persistence) {
+    persistence."/persist".directories = [ "/var/lib/flatpak" ];
+  };
 }
