c0a60fcb268ece5052f86445a54b4aecae286abe
이영욱 <pr@lyuk98.com>
2025-10-23 11:13:36 +0900

    Add persistent directory for fwupd

--- hosts/common/optional/fwupd.nix
+++ hosts/common/optional/fwupd.nix
@@ -1,4 +1,9 @@
+{ lib, config, ... }:
 {
   # Enable fwupd
   services.fwupd.enable = true;
+
+  # Preserve persistent data
+  environment.persistence."/persist".directories =
+    lib.optional config.services.fwupd.enable "/var/lib/fwupd";
 }
