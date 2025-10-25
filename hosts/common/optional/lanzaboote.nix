db16019365efa48e3a761cf2ab52fdb047070c1c
이영욱 <pr@lyuk98.com>
2025-10-25 10:07:32 +0900

    Inherit configurations from systemd-boot
    Lanzaboote already does so by default, so the module for declaring
    systemd-boot's options is included.

--- hosts/common/optional/lanzaboote.nix
+++ hosts/common/optional/lanzaboote.nix
@@ -5,10 +5,10 @@
   ...
 }:
 {
-  imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
-
-  # Allow modification of EFI variables
-  boot.loader.efi.canTouchEfiVariables = true;
+  imports = [
+    inputs.lanzaboote.nixosModules.lanzaboote
+    ./systemd-boot.nix
+  ];
 
   # Disable systemd-boot since it is replaced by Lanzaboote
   boot.loader.systemd-boot.enable = lib.mkForce false;
@@ -16,9 +16,6 @@
   boot.lanzaboote = {
     # Enable Lanzaboote
     enable = true;
-
-    # Limit to 10 latest generations
-    configurationLimit = lib.mkDefault 10;
 
     # Specify location of PKI bundle
     pkiBundle = "/var/lib/sbctl";
