6f6e0bdb521872d39ab2c01e00a819596b3f7066
이영욱 <pr@lyuk98.com>
2025-11-04 10:34:46 +0900

    Use NetworkManager's auto-connect mechanism

--- hosts/common/optional/iwd.nix
+++ hosts/common/optional/iwd.nix
@@ -4,6 +4,15 @@
   networking.wireless.iwd.enable = true;
   networking.wireless.enable = lib.mkForce false;
 
-  # Use iwd as wireless backend for NetworkManager
-  networking.networkmanager.wifi.backend = lib.mkIf config.networking.networkmanager.enable "iwd";
+  networking.networkmanager = {
+    # Use iwd as wireless backend for NetworkManager
+    wifi.backend = lib.mkIf config.networking.networkmanager.enable "iwd";
+
+    settings = {
+      device = {
+        # Disable iwd's auto-connect mechanism and let NetworkManager initiate connections
+        "wifi.iwd.autoconnect" = false;
+      };
+    };
+  };
 }
