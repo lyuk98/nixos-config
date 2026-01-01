b755932f589187edb6f1ba2cad5583bde6d506cc
이영욱 <pr@lyuk98.com>
2026-01-01 10:06:23 +0900

    friction: 1.0.0-rc.2 -> 1.0.0-rc.3
    With changes to the build process by upstream, patches to replace
    hardcoded paths assuming FHS are no longer needed. GN is also built
    during the process, eliminating the need for a separate derivation.

--- packages/friction/default.nix
+++ packages/friction/default.nix
@@ -21,51 +21,12 @@
   qscintilla,
   qt5,
   qtbase,
-  stdenv,
   wrapQtAppsHook,
   zlib,
 }:
-let
-  gn = stdenv.mkDerivation {
-    pname = "gn";
-    version = "0-unstable-2025-08-25";
-
-    src = fetchFromGitHub {
-      owner = "friction2d";
-      repo = "gn";
-      rev = "70a9617aad7c09642457b6296d35638b97375dad";
-      hash = "sha256-R/HL/a47swDCYM4iwAvcOkJa7qZb8hvsj9ULitgUWuk=";
-    };
-
-    nativeBuildInputs = [
-      ninja
-      python3
-    ];
-
-    configurePhase = ''
-      python build/gen.py
-    '';
-    buildPhase = ''
-      ninja --verbose -C out -j $NIX_BUILD_CORES gn
-    '';
-    installPhase = ''
-      install --verbose -D out/gn "$out/bin/gn"
-    '';
-
-    meta = {
-      description = "Meta-build system that generates build files for Ninja";
-      mainProgram = "gn";
-      homepage = "https://gn.googlesource.com/gn";
-      license = lib.licenses.bsd3;
-      platforms = lib.platforms.unix;
-    };
-  };
-
-  qtVersion = lib.versions.major qtbase.version;
-in
 clangStdenv.mkDerivation (finalAttrs: {
   pname = "friction";
-  version = "1.0.0-rc.2";
+  version = "1.0.0-rc.3";
 
   meta = {
     description = "Powerful and versatile motion graphics application";
@@ -74,7 +35,7 @@ clangStdenv.mkDerivation (finalAttrs: {
       vector and raster animations for web and video.
     '';
     homepage = "https://friction.graphics/";
-    changelog = "https://friction.graphics/releases/friction-100-rc2.html";
+    changelog = "https://friction.graphics/releases/friction-100-rc3.html";
     license = lib.licenses.gpl3;
     mainProgram = "friction";
   };
@@ -82,21 +43,10 @@ clangStdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "friction2d";
     repo = "friction";
-    rev = "e1e2c9a20a60bbaa9c0d618b88e37ce35185cd7c";
+    rev = "a49cbb9cf6f687ebacbe7445b3b07e25ef73876c";
     fetchSubmodules = true;
-    hash = "sha256-tmxzEfOy+eCe7K4Rv+bFNk0t3aD1n4iqAroB1li9vVM=";
+    hash = "sha256-JUDqjUhtYiDll7bTNmYCItT8eQHS5pV38OwqiTXKowM=";
   };
-
-  patches = [
-    ./gn-replace-executable.patch
-  ];
-
-  postPatch = ''
-    substituteInPlace src/engine/skia/third_party/freetype2/BUILD.gn \
-      --replace-fail /usr ${freetype.dev}
-    substituteInPlace src/engine/skia/third_party/harfbuzz/BUILD.gn \
-      --replace-fail /usr ${harfbuzzFull.dev}
-  '';
 
   buildInputs = [
     expat
@@ -119,7 +69,6 @@ clangStdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [
     clang
     cmake
-    gn
     ninja
     pkg-config
     python3
@@ -130,13 +79,13 @@ clangStdenv.mkDerivation (finalAttrs: {
     "-DCMAKE_BUILD_TYPE=Release"
 
     "-DFRICTION_OFFICIAL_RELEASE=ON"
-    "-DCUSTOM_BUILD=rc.2"
+    "-DCUSTOM_BUILD=rc.3"
 
     "-DGIT_COMMIT=${builtins.substring 0 8 finalAttrs.src.rev}"
     "-DGIT_BRANCH=v${lib.versions.majorMinor finalAttrs.version}"
 
     "-DQSCINTILLA_INCLUDE_DIRS=${qscintilla}/include"
     "-DQSCINTILLA_LIBRARIES_DIRS=${qscintilla}/lib"
-    "-DQSCINTILLA_LIBRARIES=qscintilla2_qt${qtVersion}"
+    "-DQSCINTILLA_LIBRARIES=qscintilla2_qt${lib.versions.major qtbase.version}"
   ];
 })
