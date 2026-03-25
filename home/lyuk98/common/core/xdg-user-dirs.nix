8b81afa29bdddbd61c020b7900c06418e66ce3ed
이영욱 <pr@lyuk98.com>
2026-03-25 23:11:17 +0900

    Disable legacy behaviour for XDG user directories

--- home/lyuk98/common/core/xdg-user-dirs.nix
+++ home/lyuk98/common/core/xdg-user-dirs.nix
@@ -1,4 +1,7 @@
 {
   # Enable management of XDG user directories
   xdg.userDirs.enable = true;
+
+  # Do not enable legacy behaviour of setting environment variables for XDG user directories
+  xdg.userDirs.setSessionVariables = false;
 }
