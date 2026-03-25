ad660b3ad079b6183e19165d223a78d7d0d529c3
이영욱 <pr@lyuk98.com>
2026-03-26 00:09:18 +0900

    friction: drop
    Since it is now available at Nixpkgs, I do not feel the need to maintain
    one on my own.

--- packages/default.nix
+++ packages/default.nix
@@ -7,7 +7,6 @@
   metadata-to-csv = pkgs.callPackage ./metadata-to-csv { };
 
   # Packages from the internet
-  friction = pkgs.libsForQt5.callPackage ./friction { };
   openvehiclediag = pkgs.callPackage ./openvehiclediag { };
   cbf-parser = pkgs.callPackage ./openvehiclediag { program = "cbf_parser"; };
 }
