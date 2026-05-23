7236f0ea825a5bc780d06b5c2c1476f25937bca2
이영욱 <pr@lyuk98.com>
2026-05-23 08:06:58 -0400

    Revert "Specify Tailscale DNS resolver"
    This reverts commit 86c01db4ca7d018e5c0c6f0b7120080e62ab3b0e.

--- hosts/common/core/systemd-resolved.nix
+++ hosts/common/core/systemd-resolved.nix
@@ -1,26 +1,20 @@
 { lib, config, ... }:
 let
   # Fallback DNS servers
-  fallbackDns =
-    # Add Quad100 DNS resolver if Tailscale is enabled
-    (lib.optionals config.services.tailscale.enable [
-      "100.100.100.100"
-      "fd7a:115c:a1e0::53"
-    ])
-    ++ [
-      "1.1.1.1#cloudflare-dns.com"
-      "8.8.8.8#dns.google"
-      "9.9.9.9#dns.quad9.net"
-      "1.0.0.1#cloudflare-dns.com"
-      "8.8.4.4#dns.google"
-      "149.112.112.112#dns.quad9.net"
-      "2606:4700:4700::1111#cloudflare-dns.com"
-      "2001:4860:4860::8888#dns.google"
-      "2620:fe::fe#dns.quad9.net"
-      "2606:4700:4700::1001#cloudflare-dns.com"
-      "2001:4860:4860::8844#dns.google"
-      "2620:fe::9#dns.quad9.net"
-    ];
+  fallbackDns = [
+    "1.1.1.1#cloudflare-dns.com"
+    "8.8.8.8#dns.google"
+    "9.9.9.9#dns.quad9.net"
+    "1.0.0.1#cloudflare-dns.com"
+    "8.8.4.4#dns.google"
+    "149.112.112.112#dns.quad9.net"
+    "2606:4700:4700::1111#cloudflare-dns.com"
+    "2001:4860:4860::8888#dns.google"
+    "2620:fe::fe#dns.quad9.net"
+    "2606:4700:4700::1001#cloudflare-dns.com"
+    "2001:4860:4860::8844#dns.google"
+    "2620:fe::9#dns.quad9.net"
+  ];
 
   # Use DNS-over-TLS whenever possible
   dnsovertls = "opportunistic";
