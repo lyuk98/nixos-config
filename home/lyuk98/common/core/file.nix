0a7721e559fb7c139fc60bb41a763b8eb00a3d5a
이영욱 <pr@lyuk98.com>
2026-07-12 15:29:44 -0400

    Temporarily override pkgs.file
    Add patch to work around landlock hardening

--- home/lyuk98/common/core/file.nix
+++ home/lyuk98/common/core/file.nix
@@ -1,5 +1,19 @@
 { pkgs, ... }:
 {
+  # Manually override pkgs.file until the merged PR reaches nixos-unstable
+  # https://github.com/NixOS/nixpkgs/pull/540742
+  nixpkgs.overlays = [
+    (final: prev: {
+      file = prev.file.overrideAttrs (oldAttrs: {
+        postPatch = ''
+          substituteInPlace src/landlock.c --replace-fail \
+            "LANDLOCK_ACCESS_FS_READ_FILE | LANDLOCK_ACCESS_FS_READ_DIR" \
+            "LANDLOCK_ACCESS_FS_READ_FILE | LANDLOCK_ACCESS_FS_READ_DIR | LANDLOCK_ACCESS_FS_EXECUTE"
+        '';
+      });
+    })
+  ];
+
   # Add file to environment
   home.packages = [ pkgs.file ];
 }
